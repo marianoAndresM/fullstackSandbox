1ca81a153ea6261b16828484b6aba29175c0ba47
Mariano <mariano.a.messana@gmail.com>
2022-01-05 20:07:59 +0100

    Cuarto commit a heroku

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
-
 const cors = require('cors')
 
+app.use(express.static('build'))
 app.use(cors())
-
 app.use(express.json())
 
 let notes = [
